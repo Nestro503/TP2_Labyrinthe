2febc29bab963b7766915190f8f2caa21cb62fac
Nestro503 <116881924+Nestro503@users.noreply.github.com>
2023-10-04 13:48:11 +0200

    test

--- TP2_Labyrinthe/cmake-build-debug/cmake_install.cmake
+++ TP2_Labyrinthe/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Erwan/CLionProjects/TP2_Labyrinthe
+# Install script for directory: C:/Users/Erwan/CLionProjects/TP2/TP2_Labyrinthe
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Erwan/CLionProjects/TP2_Labyrinthe/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Erwan/CLionProjects/TP2/TP2_Labyrinthe/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
