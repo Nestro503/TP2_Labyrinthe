b5fb70f69bebd7cc438e393f3a0293fcdceaf00f
ethanECE <ethan.petain@edu.ece.fr>
2023-10-10 20:11:18 +0200

    cc fixé

--- TP2_Labyrinthe/bfs/bfs.c
+++ TP2_Labyrinthe/bfs/bfs.c
@@ -6,14 +6,13 @@
 
 
 void coloration_successeur(Graphe graphe, int num, File file){ // fonction qui colorie les sommets adjacents au sommet courant
-    pArc arc = graphe.pSommet[num]->arc;
+    pArc arc = graphe.pSommet[num]->arc;  // prends l'arc du sommet defiler
 
     while(arc != NULL){
         if(graphe.pSommet[arc->sommet]->couleur == 'B') {
             graphe.pSommet[arc->sommet]->couleur = 'G';
 
             enfiler(file, arc->sommet);
-            //printf("\n%d est successeur de %d",arc->sommet, num);
         }
         arc = arc->arc_suivant;
     }
@@ -54,13 +53,16 @@ void variante_algo_bsf(Graphe graphe, int sommet_initial){ // fonction qui prend
         s = defilement(file); // defiler l'élément en tête de file
         coloration_successeur(graphe, s, file);
         graphe.pSommet[s]->couleur = 'N'; // mettre le sommet courant à noir
-        printf("%d ",s);
+        printf("%d%c ",s, graphe.pSommet[s]->couleur);
     }
 }
 
 
 void recherche_CC(Graphe graphe){
     int num = 1; // compteur des composantes
+    for (int i = 0; i < graphe.ordre; ++i) {
+        graphe.pSommet[i]->couleur = 'B';
+    }
     for (int i = 0; i < graphe.ordre; ++i) { // Boucle parcourant tout le graphe
         if(graphe.pSommet[i]->couleur == 'B'){ // Tant qu'il existe d'autres sommets blanc c'est qu'il y a d'autres composantes
             printf("Composante connexe n%d : ",num); // Affichage des différentes composantes
